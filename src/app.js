c3b44543dad115e24892c37af906354784f62be2
René Olivo <minet.ws@gmail.com>
2016-10-14 19:48:03 -0400

    add cat photos

--- src/app.js
+++ src/app.js
@@ -3,8 +3,24 @@ import Pixabay from './services/pixabay';
 
 @inject(Pixabay)
 export class App {
+  selectedCat = {};
+
   constructor(pixabay: Pixabay) {
     this.pixabay = pixabay;
     this.message = 'Hello Cats!';
+
+    this.getAllTheCats();
+  }
+
+  getAllTheCats() {
+    this.pixabay.search('cats')
+    .then((results) => results.hits)
+    .then((cats) => cats.slice(0, 20))
+    .then((cats) => this.cats = cats);
+  }
+
+  openCat(cat) {
+    this.selectedCat = cat;
+    this.reneModal.open();
   }
 }
